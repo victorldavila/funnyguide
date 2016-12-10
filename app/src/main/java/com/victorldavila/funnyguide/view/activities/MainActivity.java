1c6df16d67411d0fe0a3a69023dd64cf66bd84b6
Victor D'Ávila <victorldavila@gmail.com>
2016-12-10 18:45:52 -0200

    - Fragmen manager for change. - bottom navigation finish.

--- app/src/main/java/com/victorldavila/funnyguide/view/activities/MainActivity.java
+++ app/src/main/java/com/victorldavila/funnyguide/view/activities/MainActivity.java
@@ -14,6 +14,7 @@ import com.victorldavila.funnyguide.api.TmdbAPI;
 import com.victorldavila.funnyguide.models.Genre;
 import com.victorldavila.funnyguide.view.OnViewListener;
 import com.victorldavila.funnyguide.view.presenters.GenrePresenter;
+import com.victorldavila.funnyguide.view.presenters.MainPresenter;
 
 import java.util.ArrayList;
 
@@ -21,12 +22,19 @@ import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    private static final int MAIN_FRAGMENT = 0;
+
+    private MainPresenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        presenter = new MainPresenter(this);
+        presenter.fragmentChange(MAIN_FRAGMENT);
     }
 
     private void initViews() {
@@ -36,18 +44,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_movies:
-
-                break;
-            case R.id.action_series:
-
-                break;
-            case R.id.action_profile:
-
-                break;
-        }
-
-        return false;
+        return presenter.fragmentChange(item.getItemId());
     }
 }
--- app/src/main/java/com/victorldavila/funnyguide/view/presenters/MainPresenter.java
+++ app/src/main/java/com/victorldavila/funnyguide/view/presenters/MainPresenter.java
@@ -1,8 +1,71 @@
 package com.victorldavila.funnyguide.view.presenters;
 
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.victorldavila.funnyguide.R;
+import com.victorldavila.funnyguide.view.fragments.MovieFragment;
+
 /**
  * Created by victo on 10/12/2016.
  */
 
 public class MainPresenter {
+
+    private final String nameClass = this.getClass().getSimpleName();
+
+    private static final String MOVIE_NAME = "MOVIE";
+    private static final String SERIES_NAME = "SERIES";
+    private static final String PROFILE_NAME = "PROFILE";
+
+    Context context;
+
+    public MainPresenter(Context context) {
+        this.context = context;
+    }
+
+    public boolean fragmentChange(int id) {
+        Log.d(nameClass, "Change fragment bottom navigation");
+
+        Fragment fragment = null;
+        String fragmentName = null;
+
+        switch (id) {
+            case R.id.action_movies:
+                fragmentName = MOVIE_NAME;
+                fragment = MovieFragment.newInstance();
+                break;
+            case R.id.action_series:
+                fragmentName = SERIES_NAME;
+                fragment = MovieFragment.newInstance();
+                break;
+            case R.id.action_profile:
+                fragmentName = PROFILE_NAME;
+                fragment = MovieFragment.newInstance();
+                break;
+            default:
+                fragmentName = MOVIE_NAME;
+                fragment = MovieFragment.newInstance();
+                break;
+        }
+
+        if(fragment != null) {
+            FragmentManager fm = ((AppCompatActivity) context).getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.content, fragment);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            // Add to backstack
+            ft.addToBackStack(fragmentName);
+            ft.commit();
+
+            return true;
+        }
+
+        return false;
+    }
 }
